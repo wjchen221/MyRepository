d87b1d2eb5261dcce7e1f73bb7dcad9ed8cce3d9
cwj <wjchen221@163.com>
2020-03-18 17:43:00 +0800

    创建branch2的分支

--- src/main/test.java
+++ src/main/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println(" My1 test of Git11");
 		System.out.println("CREATE NEW BRANCH");
+		System.out.println("create branck2");
 	}
 }
